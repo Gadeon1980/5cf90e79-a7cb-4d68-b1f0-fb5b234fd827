90b3eebcb6d8dc176e7b7e354dfcd104c417ae72
jkroacar <jkroacar@gmail.com>
2017-08-18 17:17:42 -0500

    Creacion del Proyecto

---
+++ TresChiflados.LinaBar/TresChiflados.LinaBar.Modelo/Entidades/LinaBarContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using TresChiflados.LinaBar.Modelo.Migrations;
+
+namespace TresChiflados.LinaBar.Modelo.Entidades
+{
+    public class LinaBarContext : DbContext
+    {
+        public LinaBarContext() : base("LinaBarContext")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<LinaBarContext, Configuration>("LinaBarContext"));
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
